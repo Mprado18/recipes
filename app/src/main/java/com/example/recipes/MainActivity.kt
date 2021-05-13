8a49b02817fa86c067e64a473819b771b052a01c
Marcos-Prado <marcosprprado@yahoo.com.br>
2021-05-13 15:47:48 -0300

    removed recyclerview for fragment unit

--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -2,37 +2,11 @@ package com.example.recipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recipes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-
-    private val recipesList: MutableList<Recipes> = mutableListOf(
-        Recipes("Receita - 1"),
-        Recipes("Receita - 2"),
-        Recipes("Receita - 3"),
-        Recipes("Receita - 4"),
-        Recipes("Receita - 5"),
-        Recipes("Receita - 6"),
-        Recipes("Receita - 7"),
-        Recipes("Receita - 8"),
-        Recipes("Receita - 9"),
-        Recipes("Receita - 10"),
-        Recipes("Receita - 11"),
-        Recipes("Receita - 12"),
-        Recipes("Receita - 13"),
-        Recipes("Receita - 14"),
-        Recipes("Receita - 15"),
-        Recipes("Receita - 16"),
-        Recipes("Receita - 17"),
-        Recipes("Receita - 18"),
-        Recipes("Receita - 19"),
-        Recipes("Receita - 20")
-    )
-
-    lateinit var recipesListAdapter: RecipesListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,8 +14,13 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        recipesListAdapter = RecipesListAdapter(this, recipesList)
-        binding.recyclerviewMain.adapter = recipesListAdapter
-        binding.recyclerviewMain.layoutManager = LinearLayoutManager(this)
+        loadList(FragmentList())
+    }
+
+    private fun loadList(frag1: FragmentList) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment, frag1)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
